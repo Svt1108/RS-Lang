ed522e26b248ad0bbb9fc19d85e44dedbaa13534
svt1108 <svt_1108@inbox.ru>
2022-08-17 19:18:01 +0300

    feat: add main page design

--- src/components/view/AppView.ts
+++ src/components/view/AppView.ts
@@ -28,20 +28,23 @@ export class AppView {
   hideFooter() {}
 
   renderHeader() {
-    return `<header >
+    return `<header class = "header-lang">
     <ul id="dropdown1" class="dropdown-content">
-    <li><a href="#!">Dictionary</a></li>
-    <li><a href="#!">Games</a></li>
+    <li><a href="#!">На главную</a></li>
     <li class="divider"></li>
-    <li><a href="#!">Statistics</a></li>
+    <li><a href="#!">Словарь</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">Игры</a></li>
+    <li class="divider"></li>
+    <li><a href="#!">Статистика</a></li>
   </ul>
-  <nav class = "teal darken-4 nav-lang">
+  <nav class = "nav-lang">
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">English train<i class="material-icons">train</i></a>
+      <a href="#!" class="brand-logo logo-lang"></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="sass.html">Login</a></li>
         <!-- Dropdown Trigger -->
-        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Меню<i class="material-icons right">arrow_drop_down</i></a></li>
       </ul>
     </div>
   </nav>
@@ -49,40 +52,29 @@ export class AppView {
   }
 
   renderFooter() {
-    return `  <footer class="page-footer teal darken-4">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
-        </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    return `<footer class="page-footer page-footer-lang">
+    <span id="contacts"></span>
     <div class="footer-copyright">
-      <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-      </div>
+        <div class="container">
+            <a href="https://rs.school/js/" class="grey-text text-lighten-4 left">
+                <div class="page-footer__logo"></div>
+            </a>
+            <div class="page-footer__github-wrap right">
+            <a href="https://github.com/Svt1108">
+            <div class="page-footer__github"></div>
+        </a>
+        <p>Alexander Krasovskiy</p>
+            <a href="https://github.com/Svt1108">
+            <div class="page-footer__github"></div>
+        </a>
+        <p>Alla Tkachuk</p>
+                <a href="https://github.com/Svt1108">
+                    <div class="page-footer__github"></div>
+                </a>
+                <p>Svetlana Pozina, 2022</p>
+            </div>
+        </div>
     </div>
-  </footer>`;
+</footer>`;
   }
 }
--- src/components/view/MainView.ts
+++ src/components/view/MainView.ts
@@ -8,22 +8,19 @@ export class MainView {
   }
 
   render() {
-    this.mainDiv.innerHTML = `  <div id="index-banner" class="parallax-container">
+    this.mainDiv.innerHTML = `<div id="index-banner" class="parallax-container valign-wrapper top-img-lang">
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
-        <h1 class="header center white-text h1-lang">English train</h1>
+        <h2 class="header center white-text h1-lang">English train</h2>
         <div class="row center">
-          <h5 class="header light h5-lang">The friendliest app for learning english</h5>
-        </div>
-        <div class="row center">
-          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light grey lighten-3 btn-lang">Get Started</a>
+          <h5 class="header light h5-lang">Твой друг в мире английского языка</h5>
         </div>
         <br><br>
 
       </div>
     </div>
-       <div class="parallax"><img src="assets/images/train.jpg" alt="Unsplashed background img 1" class = "img-parallax"></div>
+       <div class="parallax"><img src="assets/images/train.jpg" alt="train" class = "img-parallax"></div>
   </div>
 
 
@@ -34,9 +31,12 @@ export class MainView {
       <div class="row">
       <div class="col s12 center">
         <h3><i class="mdi-content-send brown-text"></i></h3>
-        <h4>About Application</h4>
+        <h4>О приложении</h4>
         <p class="left-align light main-text-lang">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
       </div>
+      <div class="row center">
+      <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light grey lighten-3 z-depth-3 btn-lang">Поехали!</a>
+    </div>
     </div>
 
     </div>
@@ -47,22 +47,22 @@ export class MainView {
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center">
-          <h5 class="header light h5-lang">The friendliest app for learning english</h5>
+          <h5 class="header light h5-lang">Твой друг в мире английского языка</h5>
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="assets/images/big-ben-2.jpg" alt="Unsplashed background img 2" class = "img-parallax"></div>
+    <div class="parallax"><img src="assets/images/big-ben-2.jpg" alt="big ben" class = "img-parallax"></div>
   </div>
 
   <div class="container">
     <div class="section">
 
       <div class="row">
-      <h4 class="center">About Us</h4>
+      <h4 class="center">О нас</h4>
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Alexander</h5>
+            <h2 class="center brown-text"><i class="material-icons">directions_bike</i></h2>
+            <h5 class="center">Александр</h5>
 
             <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
           </div>
@@ -70,8 +70,8 @@ export class MainView {
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">Alla</h5>
+            <h2 class="center brown-text"><i class="material-icons">nature_people</i></h2>
+            <h5 class="center">Алла</h5>
 
             <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
           </div>
@@ -79,8 +79,8 @@ export class MainView {
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">Svetlana</h5>
+            <h2 class="center brown-text"><i class="material-icons">rowing</i></h2>
+            <h5 class="center">Светлана</h5>
 
             <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
           </div>
@@ -95,11 +95,11 @@ export class MainView {
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center">
-          <h5 class="header light h5-lang">The friendliest app for learning english</h5>
+          <h5 class="header light h5-lang">Твой друг в мире английского языка</h5>
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="assets/images/castle.jpg" alt="Unsplashed background img 3" class = "img-parallax"></div>
+    <div class="parallax"><img src="assets/images/castle.jpg" alt="castle" class = "img-parallax"></div>
   </div>`;
     // const x = this.renderX
     // const y = this.renderY
