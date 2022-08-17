6cbdd304fdbebced264016a4005ab48f4b51b686
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-17 17:59:51 +0500

    fix: eslint problem

--- src/components/init/init.js
+++ src/components/init/init.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
-(function($) {
-    $(function() {
-        $('.sidenav').sidenav();
-        $('.parallax').parallax();
-        $('.dropdown-trigger').dropdown();
-    }); // end of document ready
+(function a($) {
+  $(() => {
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
