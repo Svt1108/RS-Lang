5e956a7549b255346e0f498087ad72e3c55998b8
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-17 17:07:02 +0500

    refactor: reorder high-level imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import './scss/global.scss';
+import './components/init/init';
 import 'materialize-css/dist/js/materialize';
+import './scss/global.scss';
 
 import { AppController } from './components/controller/AppController';
-
-// import './components/init/jquery-2.1.1.min.js';
-import './components/init/init';
 
 const app = new AppController();
 app.start();
