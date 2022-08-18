78ff3a76a5a05b05652025ba897e7a281801af2c
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-18 16:41:03 +0500

    feat: appController - add dragGame and update learnBook

--- src/components/controller/AppController.ts
+++ src/components/controller/AppController.ts
@@ -20,53 +20,63 @@ export class AppController {
     // this.book = new BookController(this.mainDiv);
     // this.audio = new AudioGameController(this.mainDiv);
     // this.sprint = new SprintGameController(this.mainDiv);
+    // this.drag = new DragAndDropController(this.mainDiv);
     // this.stats = new StatsController(this.mainDiv);
   }
 
   public start() {
-    const [page, num] = window.location.hash.slice(1).split('#'); // [page, num] // [ '', undefined ]
-    this.appView.render(page); // for footer display
+    const [route, difficulty, page] = window.location.hash.slice(1).split('#');
+    // simple - ['auth', undefined, undefined]
+    // game - ['sprint', '3', undefined]
+    // book - ['book', '2', '19']
+    this.appView.render(route); // for footer display
 
-    this.renderNewPage([page, num]);
+    this.renderNewPage([route, difficulty, page]);
     this.enableRouting();
   }
 
   private enableRouting() {
     window.addEventListener('hashchange', () => {
-      const [page, num] = window.location.hash.slice(1).split('#');
-      this.renderNewPage([page, num]);
+      const [route, difficulty, page] = window.location.hash.slice(1).split('#');
+      this.renderNewPage([route, difficulty, page]);
     });
   }
 
-  private renderNewPage([page, num = '']: string[]) {
+  private renderNewPage([route, difficulty = '', page = '']: string[]) {
     this.mainDiv.innerHTML = '';
-    console.log('del log from AppController', num);
+    console.log('del log from AppController', difficulty, page);
 
-    if (page === 'main' || page === '') {
+    if (route === 'main' || route === '') {
       this.main.show();
       this.appView.showFooter();
-    } else if (page === 'auth') {
+    } else if (route === 'auth') {
       // this.auth.show();
       this.appView.showFooter();
-    } else if (page === 'book') {
-      // num ?
-      // this.book.show(Number(num)) :
+    } else if (route === 'book') {
+      // difficulty ?
+      // this.book.show(Number(difficulty), Number(page)) :
       // this.book.show();
       //
       // this.appView.showFooter();
-    } else if (page === 'audio') {
-      // num ?
-      // this.audio.showGame(Number(num)) :
-      // this.audio.showSettings();
+    } else if (route === 'audio') {
+      // difficulty ?
+      // this.audio.show(Number(difficulty)) :
+      // this.audio.show();
       //
       // this.appView.hideFooter();
-    } else if (page === 'sprint') {
-      // num ?
-      // this.sprint.showGame(Number(num)) :
-      // this.sprint.showSettings();
+    } else if (route === 'sprint') {
+      // difficulty ?
+      // this.sprint.show(Number(difficulty)) :
+      // this.sprint.show();
       //
       // this.appView.hideFooter();
-    } else if (page === 'stats') {
+    } else if (route === 'drag') {
+      // difficulty ?
+      // this.drag.showGame(Number(difficulty)) :
+      // this.drag.showSettings();
+      //
+      // this.appView.hideFooter();
+    } else if (route === 'stats') {
       // this.stats.show();
       // this.appView.showFooter();
     } else {
