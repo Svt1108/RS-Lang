994db618927f448d1e1f26172d3c271bcf991818
svt1108 <svt_1108@inbox.ru>
2022-08-20 21:19:23 +0300

    fix: use createElement for id

--- src/components/view/BookView.ts
+++ src/components/view/BookView.ts
@@ -11,8 +11,7 @@ export class BookView {
   render(res: Word[]) {
     console.log(res);
 
-    const bookWrap = createElement('div', 'book-wrap');
-    bookWrap.id = 'book-wrap';
+    const bookWrap = createElement('div', 'book-wrap', '', 'book-wrap');
     this.mainDiv.appendChild(bookWrap);
     const title = createElement('div', 'parallax-container valign-wrapper title-lang');
     title.innerHTML = `    <div class="section no-pad-bot">
@@ -58,35 +57,40 @@ export class BookView {
     const levelsTitle = createElement('h6', 'levels-title center', 'Разделы');
     levels.appendChild(levelsTitle);
 
-    const level1 = createElement('div', 'level-btn z-depth-1 waves-effect waves-purple', 'Level 1');
-    level1.style.border = `solid 1px #7851A9`;
+    const level0 = createElement('div', 'level-btn z-depth-1 waves-effect waves-purple', 'Level 0');
+    level0.style.border = `solid 1px #7851A9`;
+    levels.appendChild(level0);
+    level0.onclick = () => this.switchImages(0);
+
+    const level1 = createElement('div', 'level-btn z-depth-1 waves-effect waves-yellow', 'Level 1');
+    level1.style.border = `solid 1px #F0E891`;
     levels.appendChild(level1);
-    level1.onclick = () => this.switchImages(0);
+    level1.onclick = () => this.switchImages(1);
 
-    const level2 = createElement('div', 'level-btn z-depth-1 waves-effect waves-yellow', 'Level 2');
-    level2.style.border = `solid 1px #F0E891`;
+    const level2 = createElement('div', 'level-btn z-depth-1 waves-effect waves-green', 'Level 2');
+    level2.style.border = `solid 1px #317F43`;
     levels.appendChild(level2);
+    level2.onclick = () => this.switchImages(2);
 
-    const level3 = createElement('div', 'level-btn z-depth-1 waves-effect waves-green', 'Level 3');
-    level3.style.border = `solid 1px #317F43`;
+    const level3 = createElement('div', 'level-btn z-depth-1 waves-effect waves-teal', 'Level 3');
+    level3.style.border = `solid 1px #1CA9C9`;
     levels.appendChild(level3);
-    level3.onclick = () => this.switchImages(2);
+    level3.onclick = () => this.switchImages(3);
 
-    const level4 = createElement('div', 'level-btn z-depth-1 waves-effect waves-teal', 'Level 4');
-    level4.style.border = `solid 1px #1CA9C9`;
+    const level4 = createElement('div', 'level-btn z-depth-1 waves-effect waves-orange', 'Level 4');
+    level4.style.border = `solid 1px #F9991A`;
     levels.appendChild(level4);
+    level4.onclick = () => this.switchImages(4);
 
-    const level5 = createElement('div', 'level-btn z-depth-1 waves-effect waves-orange', 'Level 5');
-    level5.style.border = `solid 1px #F9991A`;
+    const level5 = createElement('div', 'level-btn z-depth-1 waves-effect waves-red', 'Level 5');
+    level5.style.border = `solid 1px #9B111E`;
     levels.appendChild(level5);
+    level5.onclick = () => this.switchImages(5);
 
-    const level6 = createElement('div', 'level-btn z-depth-1 waves-effect waves-red', 'Level 6');
-    level6.style.border = `solid 1px crimson`;
+    const level6 = createElement('div', 'level-btn z-depth-1 waves-effect waves-light', 'Level 6');
+    level6.style.border = `solid 1px #FCFCFD`;
     levels.appendChild(level6);
-
-    const level7 = createElement('div', 'level-btn z-depth-1 waves-effect waves-light', 'Level 7');
-    level7.style.border = `solid 1px #FCFCFD`;
-    levels.appendChild(level7);
+    level6.onclick = () => this.switchImages(6);
   }
 
   switchImages(level: number) {
@@ -96,7 +100,12 @@ export class BookView {
     const bookWrap = document.getElementById('book-wrap') as HTMLElement;
 
     if (level === 0) color = 'violet';
+    if (level === 1) color = 'yellow';
     if (level === 2) color = 'green';
+    if (level === 3) color = 'blue';
+    if (level === 4) color = 'orange';
+    if (level === 5) color = 'red';
+    if (level === 6) color = 'white';
 
     picture1.src = `assets/images/${color}-1.jpg`;
     picture3.src = `assets/images/${color}-3.jpg`;
