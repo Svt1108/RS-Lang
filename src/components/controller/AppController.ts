fd98a4dd853f5540d0763d1d8247199e95878e33
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-20 12:25:17 +0500

    feat: add Routes enum

--- src/components/controller/AppController.ts
+++ src/components/controller/AppController.ts
@@ -2,6 +2,7 @@ import { AppView } from '../view/AppView';
 import { AppModel } from '../model/AppModel';
 import { MainController } from './MainController';
 import { createElement } from '../view/helpers/renderHelpers';
+import { Route } from '../types/appRoutes';
 
 export class AppController {
   mainDiv;
@@ -46,37 +47,37 @@ export class AppController {
     this.mainDiv.innerHTML = '';
     console.log('del log from AppController', route, level, page);
 
-    if (route === 'main' || route === '') {
+    if (route === Route.main || route === '') {
       this.main.show();
       this.appView.showFooter();
-    } else if (route === 'login') {
+    } else if (route === Route.login) {
       // this.auth.show();
       this.appView.showFooter();
-    } else if (route === 'book') {
+    } else if (route === Route.book) {
       // level ?
       // this.book.show(Number(level), Number(page)) :
       // this.book.show();
       //
       this.appView.showFooter();
-    } else if (route === 'audio') {
+    } else if (route === Route.audio) {
       // level ?
       // this.audio.show(Number(level)) :
       // this.audio.show();
       //
       this.appView.hideFooter();
-    } else if (route === 'sprint') {
+    } else if (route === Route.sprint) {
       // level ?
       // this.sprint.show(Number(level)) :
       // this.sprint.show();
       //
       this.appView.hideFooter();
-    } else if (route === 'drag') {
+    } else if (route === Route.drag) {
       // level ?
       // this.drag.showGame(Number(level)) :
       // this.drag.showSettings();
       //
       this.appView.hideFooter();
-    } else if (route === 'stats') {
+    } else if (route === Route.stats) {
       // this.stats.show();
       this.appView.showFooter();
     } else {
