411b2b7936d7b60fbd23c76f0e4a4ac1144d5aa2
svt1108 <svt_1108@inbox.ru>
2022-08-21 10:00:04 +0300

    feat: add game buttons

--- src/components/view/BookView.ts
+++ src/components/view/BookView.ts
@@ -9,8 +9,6 @@ export class BookView {
   }
 
   render(res: Word[]) {
-    console.log(res);
-
     const bookWrap = createElement('div', 'book-wrap', '', 'book-wrap');
     this.mainDiv.appendChild(bookWrap);
     const title = createElement('div', 'parallax-container valign-wrapper title-lang');
@@ -32,14 +30,15 @@ export class BookView {
 
     const levels = createElement('div', 'col s12 m2 levels');
     row.appendChild(levels);
-
     this.renderLevels(levels);
 
     const cards = createElement('div', 'col s12 m8 cards');
     row.appendChild(cards);
+    this.renderCards(cards, res);
 
-    const games = createElement('div', 'col s12 m2');
+    const games = createElement('div', 'col s12 m2 games');
     row.appendChild(games);
+    this.renderGames(games);
 
     const pagination = createElement('div', 'pagination');
     bookWrap.appendChild(pagination);
@@ -55,40 +54,76 @@ export class BookView {
     const levelsTitle = createElement('h6', 'levels-title center', 'Разделы');
     levels.appendChild(levelsTitle);
 
-    const level0 = createElement('div', 'level-btn z-depth-1 waves-effect waves-purple', 'Level 0');
+    const level0 = createElement('div', 'level-btn z-depth-1 waves-effect waves-purple', 'LEVEL 0');
     level0.style.border = `solid 1px #7851A9`;
+    level0.style.borderLeft = `solid 3px #7851A9`;
     levels.appendChild(level0);
     level0.onclick = () => this.switchImages(0);
 
-    const level1 = createElement('div', 'level-btn z-depth-1 waves-effect waves-yellow', 'Level 1');
+    const level1 = createElement('div', 'level-btn z-depth-1 waves-effect waves-yellow', 'LEVEL 1');
     level1.style.border = `solid 1px #F0E891`;
+    level1.style.borderLeft = `solid 3px #F0E891`;
     levels.appendChild(level1);
     level1.onclick = () => this.switchImages(1);
 
-    const level2 = createElement('div', 'level-btn z-depth-1 waves-effect waves-green', 'Level 2');
-    level2.style.border = `solid 1px #317F43`;
+    const level2 = createElement('div', 'level-btn z-depth-1 waves-effect waves-green', 'LEVEL 2');
+    level2.style.border = `solid 1px #386646`;
+    level2.style.borderLeft = `solid 3px #386646`;
     levels.appendChild(level2);
     level2.onclick = () => this.switchImages(2);
 
-    const level3 = createElement('div', 'level-btn z-depth-1 waves-effect waves-teal', 'Level 3');
-    level3.style.border = `solid 1px #1CA9C9`;
+    const level3 = createElement('div', 'level-btn z-depth-1 waves-effect waves-teal', 'LEVEL 3');
+    level3.style.border = `solid 1px #4169E1`;
+    level3.style.borderLeft = `solid 3px #4169E1`;
     levels.appendChild(level3);
     level3.onclick = () => this.switchImages(3);
 
-    const level4 = createElement('div', 'level-btn z-depth-1 waves-effect waves-orange', 'Level 4');
-    level4.style.border = `solid 1px #F9991A`;
+    const level4 = createElement('div', 'level-btn z-depth-1 waves-effect waves-orange', 'LEVEL 4');
+    level4.style.border = `solid 1px #DEB768`;
+    level4.style.borderLeft = `solid 3px #DEB768`;
     levels.appendChild(level4);
     level4.onclick = () => this.switchImages(4);
 
-    const level5 = createElement('div', 'level-btn z-depth-1 waves-effect waves-red', 'Level 5');
+    const level5 = createElement('div', 'level-btn z-depth-1 waves-effect waves-red', 'LEVEL 5');
     level5.style.border = `solid 1px #9B111E`;
+    level5.style.borderLeft = `solid 3px #9B111E`;
     levels.appendChild(level5);
     level5.onclick = () => this.switchImages(5);
 
-    const level6 = createElement('div', 'level-btn z-depth-1 waves-effect waves-light', 'Level 6');
+    const level6 = createElement('div', 'level-btn z-depth-1 waves-effect waves-light', 'СЛОЖНЫЕ СЛОВА');
     level6.style.border = `solid 1px #FCFCFD`;
+    level6.style.borderLeft = `solid 3px #FCFCFD`;
     levels.appendChild(level6);
     level6.onclick = () => this.switchImages(6);
+  }
+
+  renderGames(games: HTMLElement) {
+    const gamesTitle = createElement('h6', 'games-title center', 'Игры');
+    games.appendChild(gamesTitle);
+
+    const game0 = createElement('a', 'games-btn z-depth-2 waves-effect') as HTMLAnchorElement;
+    game0.href = '#sprint';
+    games.appendChild(game0);
+    const game0Title = createElement('div', 'game-title', 'Спринт');
+    game0.appendChild(game0Title);
+    const game0Picture = createElement('div', 'game-picture btn-sprint');
+    game0.appendChild(game0Picture);
+
+    const game1 = createElement('a', 'games-btn z-depth-2 waves-effect') as HTMLAnchorElement;
+    game1.href = '#audio';
+    games.appendChild(game1);
+    const game1Title = createElement('div', 'game-title', 'Аудиовызов');
+    game1.appendChild(game1Title);
+    const game1Picture = createElement('div', 'game-picture btn-audio');
+    game1.appendChild(game1Picture);
+
+    const game2 = createElement('a', 'games-btn z-depth-2 waves-effect') as HTMLAnchorElement;
+    game2.href = '#drag';
+    games.appendChild(game2);
+    const game2Title = createElement('div', 'game-title', 'Коллекции');
+    game2.appendChild(game2Title);
+    const game2Picture = createElement('div', 'game-picture btn-collection');
+    game2.appendChild(game2Picture);
   }
 
   switchImages(level: number) {
@@ -108,5 +143,9 @@ export class BookView {
     picture1.src = `assets/images/${color}-1.jpg`;
     picture3.src = `assets/images/${color}-3.jpg`;
     bookWrap.style.backgroundImage = `url(../assets/images/${color}-2.jpg)`;
+  }
+
+  renderCards(cards: HTMLElement, res: Word[]) {
+    console.log(res);
   }
 }
