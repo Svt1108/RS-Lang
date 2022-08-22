ce76a8e7e983ef23e094ec716d09c1a07528e027
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-22 14:03:42 +0500

    feat: start login register page

--- src/components/view/LoginView.ts
+++ src/components/view/LoginView.ts
@@ -6,8 +6,17 @@ export class LoginView {
   }
 
   renderLogin() {
-    this.main.innerHTML = `<h1>Login</h1>
-    <a href="#register">to Registry</a>`;
+    this.main.innerHTML = `
+<div class="lang_login">
+  <div class="lang_login_img_container">
+    <img src="./assets/images/login-door.png" class="lang_login_img">
+  </div>
+  <div class="lang_login_form_container">
+    <h2>FORM Title</h2>
+    <form>FORM DIV</form>
+  </div>
+</div>
+    `;
   }
 
   renderRegister() {
