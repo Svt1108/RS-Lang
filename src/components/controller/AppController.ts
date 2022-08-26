59256bab9511b7a3c2ecddaa2b4052be6db76a12
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-08-26 12:29:36 +0500

    fix: repair AppController after Pull & Merge Dev

--- src/components/controller/AppController.ts
+++ src/components/controller/AppController.ts
@@ -36,6 +36,7 @@ export class AppController {
     this.loader.init();
     this.enableRouting();
     this.updateLoginStatusOnFocus();
+
     await this.renderNewPage([route, level, page]);
   }
 
@@ -99,8 +100,10 @@ export class AppController {
 
   private updateLoginStatusOnFocus() {
     window.addEventListener('focus', async () => {
+      this.loader.show();
       const status = await this.login.updateLoginStatus();
       this.appView.updateLoginBtnText(status);
+      this.loader.hide();
     });
   }
 }
