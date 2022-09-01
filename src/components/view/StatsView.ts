4787c2ba7a1817d22e1940dc81f2d54721270d30
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-09-01 17:41:03 +0500

    feat: add dynamic render for today stats

--- src/components/view/StatsView.ts
+++ src/components/view/StatsView.ts
@@ -1,5 +1,12 @@
-/* eslint-disable no-new */
 import { BarChart, LineChart, easings } from 'chartist';
+import {
+  createTopImg,
+  createTodayStats,
+  createMidImg,
+  createLongStats,
+  createBottomImg,
+  createScrollSpy,
+} from './helpers/statRenderHelpers';
 import { Stats } from '../types/statsTypes';
 
 export class StatView {
@@ -12,130 +19,143 @@ export class StatView {
   render(stats: Stats) {
     console.log(stats);
 
-    this.mainDiv.innerHTML = `
-<div id="index-banner" class="parallax-container valign-wrapper top-img-stat">
+    // Render_Page
+    const topImg = createTopImg();
+    const todayStats = createTodayStats(stats);
+    const midImg = createMidImg();
+    const longStats = createLongStats();
+    const bottomImg = createBottomImg();
+    const scrollSpy = createScrollSpy();
 
-  <div class="section no-pad-bot ">
-    <div class="container">
-      <h4 class="header center white-text h4-lang">Статистика за сегодня</h4>
-    </div>
-  </div>
+    this.mainDiv.innerHTML = '';
+    this.mainDiv.append(topImg, todayStats, midImg, longStats, bottomImg, scrollSpy);
 
-  <div class="parallax">
-    <img src="assets/images/stat/circles-1.jpg" alt="circles" class = "img-parallax">
-  </div>
+    // Handle_Graphs
+    // sortLong
+    // makeDateLabelsArr
+    // makeDataSeriesArr ?
+    // count divider
+    // enable BarSeries(labels, dataArr?, divider);
+    // enable BarSeries(labels, dataArr?, divider);
 
-</div>
+    //     this.mainDiv.innerHTML = `
+    // <div id="index-banner" class="parallax-container valign-wrapper top-img-stat">
 
-<div id="day-stat" class="container scrollspy">
-  <div class="center stat-day-wrap section-stat">
-    <div class = "card z-depth-2 center stat-day">
+    //   <div class="section no-pad-bot ">
+    //     <div class="container">
+    //       <h4 class="header center white-text h4-lang">Статистика за сегодня</h4>
+    //     </div>
+    //   </div>
 
+    //   <div class="parallax">
+    //     <img src="assets/images/stat/circles-1.jpg" alt="circles" class = "img-parallax">
+    //   </div>
 
+    // </div>
 
-      <div class="stat_card">
-        <div class="stat_card_heading">
-          <span class="stat_card_img"><div class="game-picture stat_btn-total"></div></span>
-          <span class="stat_card_title">Итого по словам</span>
-        </div>
-        <div class="stats_list">
-          <div class="stat_text_container"><span>Изученных слов</span><span class="stat_num orange-text text-darken-3">5</span></div>
-          <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
-          <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">21%</span></div>
-        </div>
-      </div>
-      <div class="stat_card">
-        <div class="stat_card_heading">
-          <span class="stat_card_img"><div class="game-picture btn-sprint"></div></span>
-          <span class="stat_card_title">Спринт</span>
-        </div>
-        <div class="stats_list">
-        <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
-        <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
-        <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
-        </div>
-      </div>
-      <div class="stat_card">
-        <div class="stat_card_heading">
-          <span class="stat_card_img"><div class="game-picture btn-audio"></div></span>
-          <span class="stat_card_title">Аудио-Вызов</span>
-        </div>
-        <div class="stats_list">
-        <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
-          <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
-          <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
-        </div>
-      </div>
-      <div class="stat_card">
-        <div class="stat_card_heading">
-          <span class="stat_card_img"><div class="game-picture btn-collection"></div></span>
-          <span class="stat_card_title">Фразы</span>
-        </div>
-        <div class="stats_list">
-        <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
-          <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
-          <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
-        </div>
-      </div>
-     
+    // <div id="day-stat" class="container scrollspy">
+    //   <div class="center stat-day-wrap section-stat">
+    //     <div class = "card z-depth-2 center stat-day">
 
+    //       <div class="stat_card">
+    //         <div class="stat_card_heading">
+    //           <span class="stat_card_img"><div class="game-picture stat_btn-total"></div></span>
+    //           <span class="stat_card_title">Итого по словам</span>
+    //         </div>
+    //         <div class="stats_list">
+    //           <div class="stat_text_container"><span>Изученных слов</span><span class="stat_num orange-text text-darken-3">5</span></div>
+    //           <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
+    //           <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">21%</span></div>
+    //         </div>
+    //       </div>
+    //       <div class="stat_card">
+    //         <div class="stat_card_heading">
+    //           <span class="stat_card_img"><div class="game-picture btn-sprint"></div></span>
+    //           <span class="stat_card_title">Спринт</span>
+    //         </div>
+    //         <div class="stats_list">
+    //         <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
+    //         <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
+    //         <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
+    //         </div>
+    //       </div>
+    //       <div class="stat_card">
+    //         <div class="stat_card_heading">
+    //           <span class="stat_card_img"><div class="game-picture btn-audio"></div></span>
+    //           <span class="stat_card_title">Аудио-Вызов</span>
+    //         </div>
+    //         <div class="stats_list">
+    //         <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
+    //           <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
+    //           <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
+    //         </div>
+    //       </div>
+    //       <div class="stat_card">
+    //         <div class="stat_card_heading">
+    //           <span class="stat_card_img"><div class="game-picture btn-collection"></div></span>
+    //           <span class="stat_card_title">Фразы</span>
+    //         </div>
+    //         <div class="stats_list">
+    //         <div class="stat_text_container"><span>Лучшая серия</span><span class="stat_num light-green-text text-darken-3">8</span></div>
+    //           <div class="stat_text_container"><span>Новых слов</span><span class="stat_num">10</span></div>
+    //           <div class="stat_text_container"><span>Правильных ответов</span><span class="stat_num">47%</span></div>
+    //         </div>
+    //       </div>
 
-    </div>
-  </div>
-</div>
+    //     </div>
+    //   </div>
+    // </div>
 
-<div class="parallax-container valign-wrapper center-img-stat">
-  <div class="section no-pad-bot">
-    <div class="container">
-      <div class="row center">
-        <h4 class="header center white-text h4-lang">Статистика за все время</h4>
-      </div>
-    </div>
-  </div>
-  <div class="parallax">
-    <img src="assets/images/stat/houses.jpg" alt="big ben" class = "img-parallax">
-  </div>
-</div>
+    // <div class="parallax-container valign-wrapper center-img-stat">
+    //   <div class="section no-pad-bot">
+    //     <div class="container">
+    //       <div class="row center">
+    //         <h4 class="header center white-text h4-lang">Статистика за все время</h4>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div class="parallax">
+    //     <img src="assets/images/stat/houses.jpg" alt="big ben" class = "img-parallax">
+    //   </div>
+    // </div>
 
-<div id="all-stat" class="container scrollspy">
-  <div class="center stat-day-wrap section-stat">
-    <div class = "card z-depth-2 center stat-day">
+    // <div id="all-stat" class="container scrollspy">
+    //   <div class="center stat-day-wrap section-stat">
+    //     <div class = "card z-depth-2 center stat-day">
 
+    //       <div class="stats_charts_container">
+    //         <h4 class="stat_long_heading">Новых (уникальных) слов по дням</h4>
+    //         <div id="stats_bar_chart" class="stats_chart"></div>
+    //         <h4 class="stat_long_heading">Всего изучено слов (прогресс)</h4>
+    //         <div id="stats_line_chart" class="stats_chart"></div>
+    //       </div>
 
-      <div class="stats_charts_container">
-        <h4 class="stat_long_heading">Новых (уникальных) слов по дням</h4>
-        <div id="stats_bar_chart" class="stats_chart"></div>
-        <h4 class="stat_long_heading">Всего изучено слов (прогресс)</h4>
-        <div id="stats_line_chart" class="stats_chart"></div>
-      </div>
+    //     </div>
+    //   </div>
+    // </div>
 
+    // <div class="parallax-container valign-wrapper">
+    //   <div class="section no-pad-bot">
+    //     <div class="container">
+    //       <div class="row center">
+    //         <h5 class="header light h5-lang">А теперь на тренировку :)</h5>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div class="parallax">
+    //     <img src="assets/images/stat/hogwards.jpg" alt="castle" class = "img-parallax">
+    //   </div>
+    // </div>
 
+    // <div class="col m3 l2 scroll-stat">
+    //   <ul class="section table-of-contents">
+    //     <li><a href="#day-stat">За сегодня</a></li>
+    //     <li><a href="#all-stat">За все время</a></li>
+    //   </ul>
+    // </div>
+    // `;
 
-    </div>
-  </div>
-</div>
-
-<div class="parallax-container valign-wrapper">
-  <div class="section no-pad-bot">
-    <div class="container">
-      <div class="row center">
-        <h5 class="header light h5-lang">А теперь на тренировку :)</h5>
-      </div>
-    </div>
-  </div>
-  <div class="parallax">
-    <img src="assets/images/stat/hogwards.jpg" alt="castle" class = "img-parallax">
-  </div>
-</div>
-  
-<div class="col m3 l2 scroll-stat">
-  <ul class="section table-of-contents">
-    <li><a href="#day-stat">За сегодня</a></li>
-    <li><a href="#all-stat">За все время</a></li>
-  </ul>
-</div>
-`;
-
+    // eslint-disable-next-line no-new
     new BarChart(
       '#stats_bar_chart',
       {
@@ -173,7 +193,7 @@ export class StatView {
     // 7-15 -> 3
     // 16-20 -> 4
 
-    // (len - 1) % 5 === 0 && len > 1
+    // if (len - 1) % 5 === 0 && len > 1
     // len 6 / 5 = Math.ceil(1.2) = 2 denominator
     // len 11 / 5 = Math.ceil(2.2) = 3 divider
     // len 16 / 5 = Math.ceil(3.2) = 4 divisor
